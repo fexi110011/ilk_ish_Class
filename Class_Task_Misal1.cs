399da9fda5bc7ad4ef989f03b8eabf5bac7bab75
Abbas0vv <ilhamabbasov46@gmail.com>
2025-03-17 11:47:58 +0400

    misallarin cavablari

---
+++ Class_Task_Misal1.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            int b = 20;
+
+            Console.WriteLine($"Evvelki a ededi {a}");
+            Console.WriteLine($"Evvelki b ededi {b}");
+
+            ChangeNum(ref a,ref b );
+            Console.WriteLine($"Sonraki a ededi {a}");
+            Console.WriteLine($"Sonraki b ededi {b}");
+        }
+        static void ChangeNum(ref int a, ref int b)
+        {
+            a = 20;
+            b = 10;
+        }
+    }
+}
---
+++ Class_task_Misal2.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3 };
+
+            Console.WriteLine($"Evvelki____" + string.Join(", ", ints));
+
+            MenfiChange(ref ints);
+            Console.WriteLine($"Sonraki____" + string.Join(", ", ints));
+        }
+        static void MenfiChange(ref int[] ints)
+        {
+            for (int i = 0; i < ints.Length; i++)
+            {
+                if (ints[i] > 0)
+                {
+                    ints[i] = -ints[i];
+                }
+            }
+        }
+    }
+
+}
