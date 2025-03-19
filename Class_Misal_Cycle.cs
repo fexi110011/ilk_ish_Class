a40e77320b4e67466967c8b59b7922f7f6fedc46
fexi110011 <fakhraddinaa-ab109@code.edu.az>
2025-03-19 12:59:43 +0400

    cavblar

---
+++ Class_Misal_Cycle.cs
@@ -0,0 +1,78 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+
+
+
+
+    class Car
+    {
+        public int DoorsNum;
+        public int doorsnum
+        {
+            get
+            {
+                return DoorsNum;
+            }
+            set
+            {
+                if (doorsnum > 0 && doorsnum < 5) { DoorsNum = doorsnum; }
+                else { Console.WriteLine("doorsnum duz qeyd edin"); }
+            }
+        }
+    }
+
+
+
+    class Bicycle
+    {
+        int WheelNum;
+        string Brand;
+        string Model;
+        double Mile;
+        public string brand
+        {
+            get
+            {
+                return Brand;
+            }
+            set
+            {
+                if (brand.Length > 3 && brand.Length < 30) { Brand = brand; }
+                else { Console.WriteLine("Brand duz qeyd edin"); }
+            }
+        }
+        public string model
+        {
+            get
+            {
+                return Model;
+            }
+            set
+            {
+                if (brand.Length > 3 && brand.Length < 30) { Model = model; }
+                else { Console.WriteLine("Model duz qeyd edin"); }
+
+            }
+        }
+        public double mile
+        {
+            get
+            {
+                return Mile;
+            }
+            set
+            {
+                if (mile > 0) { Mile = mile; }
+                else { Console.WriteLine("Mile duz qeyd edin"); }
+            }
+        }
+    }
+}
---
+++ Misal_Task_Coxlu-metod.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student01 = new Student("Fexreddin_Ehmedov",109,1000);
+            Student student02 = new Student("Kenen_Mehdiyev", 109, 1200);
+            Student student03 = new Student("Abbas_Abbasov", 101, 1300);
+
+
+        }
+    }
+    class Student
+    {
+        string FullName;
+        int GroupNo;
+        int AvgPoint;
+        public Student(string fullname,int groupNo,int avgPoint)
+        {
+            FullName = fullname;
+            GroupNo = groupNo;
+            AvgPoint = avgPoint;
+        }
+       
+    }
+    class Group
+    {
+        int No;
+        string[] Students;
+        int StudentLimit;
+        public Group(int no, int studentlimit)
+        {
+            No = no;
+            StudentLimit = studentlimit;
+        }
+        public bool AddStudent(Student student) {
+            int[] new_Student;
+            for (int i = 0; Students.Length > 0; i++)
+            {
+
+            }
+            return true;
+        }
+    }
+}
